c83b3a0210dc09abe7f16734edb2ce5b82355ddd
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-06-26 21:32:53 +0900

    Retry when unable to load image data

--- bot.py
+++ bot.py
@@ -26,10 +26,12 @@ auth.set_access_token(credentials['AccessToken'],
 api = tweepy.API(auth)
 
 def do_tweet(event, context):
-    text, img, img_flavor = wtffuture.random_future()
-    f = StringIO(urllib2.urlopen(img).read())
-    api.update_with_media(img, status=text, file=f)
-    return text, img, img_flavor
+    for _ in xrange(5):
+        text, img, img_flavor = wtffuture.random_future()
+        f = StringIO(urllib2.urlopen(img).read())
+        api.update_with_media(img, status=text, file=f)
+        return text, img, img_flavor
+    raise Exception('Failed to tweet')
 
 if __name__ == '__main__':
     print(do_tweet(None, None))
